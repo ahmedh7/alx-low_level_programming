d32024cd519faa246b7a6affef7b206f511dcb47
Ahmed Hesham <A7madhesham@outlook.com>
2023-12-16 01:02:11 +0200

    alpha

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	    for (int i = 0; i <= 99; ++i) {
+		            for (int j = i; j <= 99; ++j) {
+				                putchar('0' + i / 10);
+						            putchar('0' + i % 10);
+							                putchar(' ');
+
+									            putchar('0' + j / 10);
+										                putchar('0' + j % 10);
+
+												            if (i != 99 || j != 99) {
+														                    putchar(',');
+																                    putchar(' ');
+																		                }
+													            }
+			        }
+
+	        putchar('\n');
+
+		    return 0;
+}
+
