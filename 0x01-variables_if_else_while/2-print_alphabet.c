a2fbf7d5daf6e67c24ca8ed921f69d989f49fd9e
Ahmed Hesham <A7madhesham@outlook.com>
2023-12-16 00:48:21 +0200

    last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	    char c;
+
+	// Print the alphabet using putchar
+	     for (c = 'a'; c <= 'z'; ++c)
+	             putchar(c);
+	
+	    // Print a new line using putchar
+	                     putchar('\n');
+	
+	                         return 0;
+	                         }
+	
