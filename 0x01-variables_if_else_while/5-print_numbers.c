cae6250ddaaee9e246a3f6eb998ed16f15c5e933
Ahmed Hesham <A7madhesham@outlook.com>
2023-12-16 00:53:47 +0200

    alpha

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	    char digit;
+
+	        for (digit = '0'; digit <= '9'; ++digit) {
+			        putchar(digit);
+				    }
+
+		    putchar('\n');
+
+		        return 0;
+}
+
