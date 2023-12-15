03c3676226db9f658b9ece4a00bb65d80caed9fb
Ahmed Hesham <A7madhesham@outlook.com>
2023-12-16 01:12:18 +0200

    alpha

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	    char c;
+	        for (c = '0'; c <= '9'; ++c) {
+			        putchar(c);
+				    }
+		    char cr
+			        for (cr = 'a'; cr <= 'f'; ++cr) {
+					        putchar(cr);
+						    }
+
+		        putchar('\n');
+
+			    return 0;
+}
+
