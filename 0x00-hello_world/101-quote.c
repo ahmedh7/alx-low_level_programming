7b2da353467597f589ff8764c581600f0528e769
Ahmed Hesham <A7madhesham@outlook.com>
2024-01-06 19:25:52 +0200

    stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Description: print a message to stderror
+ * @void: parameter
+ * Return: 1 (error but we want it)
+ */
+int main(void)
+{
+	    fprintf(stderr, "and that piece of art is useful\" - \
+			    Dora Korpar, 2015-10-19\n");
+	        return (1);
+}
+
