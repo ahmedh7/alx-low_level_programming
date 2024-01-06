2c2e6f08ad863efc5c1e6a369a17a3a2c00c5310
Ahmed Hesham <A7madhesham@outlook.com>
2024-01-06 19:09:32 +0200

    sizeof

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Calls puts and recieves value returned by puts after its
+ * execution.
+ * Return: Always return 0 to indicate success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
