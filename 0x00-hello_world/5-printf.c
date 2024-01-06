e0ae644ff4d95b0a1d547cfaa80436107d72e04e
Ahmed Hesham <A7madhesham@outlook.com>
2024-01-06 19:18:52 +0200

    sizeof

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  * execution.
  * Return: Always return 0 to indicate success
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
