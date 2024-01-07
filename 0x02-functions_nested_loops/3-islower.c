8a4522aca36bb9ef75d9644cb56b5a8f4c66722e
Ahmed Hesham <A7madhesham@outlook.com>
2024-01-07 04:48:15 +0200

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - Entry point
+ *
+ * Description: return 1 if lowercase, 0 otherwise
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
