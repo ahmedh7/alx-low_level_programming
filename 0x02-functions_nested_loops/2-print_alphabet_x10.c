70e3068cc54407b50ae264490f44c6a41b8e54b0
Ahmed Hesham <A7madhesham@outlook.com>
2024-01-07 04:37:08 +0200

    alphax10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: print alphabet 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	while(int i = 0; i < 10; i++)
+	{
+		printf("abcdefghijklmnopqrstuvwxyz\n");
+	}
+}
