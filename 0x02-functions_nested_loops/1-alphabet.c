ef2935df7279ad0afbc58829932d6f5b672c1865
Ahmed Hesham <A7madhesham@outlook.com>
2024-01-07 04:21:16 +0200

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet
+ * Return: Always return 0 to indicate success
+ */
+int main(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+	return (0);
+}
