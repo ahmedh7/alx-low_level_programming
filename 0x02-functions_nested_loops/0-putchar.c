5bafffa10824db92d884abb323a2cdef08f56939
Ahmed Hesham <A7madhesham@outlook.com>
2024-01-07 04:14:39 +0200

    print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print _putchar
+ * Return: Always return 0 to indicate success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return(0);
+}
